ad0ee67a8b0b7ba1dfe35531e14cc7fc460b0761
jesse <mr.bossman075@gmail.com>
2021-01-27 21:24:32 -0500

    interupts

--- leds/main.c
+++ leds/main.c
@@ -19,14 +19,14 @@ void init_timer(){
 	TCA0.SINGLE.CTRLA = TCA_SINGLE_ENABLE_bm;
 	TCA0.SINGLE.CTRLB = TCA_SINGLE_CMP2EN_bm | TCA_SINGLE_CMP1EN_bm | TCA_SINGLE_CMP0EN_bm | TCA_SINGLE_WGMODE1_bm | TCA_SINGLE_WGMODE0_bm;
 	TCA0.SINGLE.CTRLC = TCA_SINGLE_CMP2OV_bm | TCA_SINGLE_CMP1OV_bm | TCA_SINGLE_CMP0OV_bm;
-	TCA0.SINGLE.CMP0 = 128;
-	TCA0.SINGLE.CMP1 = 128;
-	TCA0.SINGLE.CMP2 = 128;
+	TCA0.SINGLE.CMP0 = 0;
+	TCA0.SINGLE.CMP1 = 0;
+	TCA0.SINGLE.CMP2 = 0;
 	TCA0.SINGLE.PER = 0xFF; // set top to 255
 }
 
 void init_AC(){
-	AC0.CTRLA = AC_ENABLE_bm |AC_HYSMODE_50mV_gc ;
+	AC0.CTRLA = AC_ENABLE_bm |AC_HYSMODE_50mV_gc |AC_INTMODE_NEGEDGE_gc ;
 	AC0.MUXCTRLA = AC_MUXPOS_PIN1_gc | AC_MUXNEG_PIN1_gc;
 	AC0.INTCTRL = 1;
 }
@@ -73,7 +73,7 @@ void Send (uint8_t b) {
 	sei();
 }
 
-uint8_t recv[4] = {0};
+uint8_t recv[3] = {0};
 volatile bool dos = true;
 int main(void)
 {
@@ -83,6 +83,9 @@ int main(void)
 	CLKCTRL_MCLKCTRLB = 0;
 	PORTB.DIR = 0b1111;
 	PORTB.OUT |= (1<<3);
+	PORTB.PIN4CTRL =  1 << 3;
+	PORTB.PIN5CTRL =  1 << 3;
+
 	init_AC();
 	init_timer();
 	initSerial();
@@ -94,11 +97,22 @@ int main(void)
 	PORTA.PIN5CTRL = 1 << 3;
 	PORTA.PIN6CTRL = 1 << 3;
 	PORTA.PIN7CTRL = 1 << 3;
-
 	sei();
     while (1) 
     {
-		for(uint8_t b = 0; b < 4; b++){
+		while(1){
+			while(dos);
+			cli();
+			AC0.INTCTRL = 0;
+			uint8_t tmp = readSerial();
+			AC0.INTCTRL = 1;
+			sei();
+			dos = true;
+				TCA0.SINGLE.CTRLESET = (1 << 2); //reset timmer
+			if(tmp == ((PORTC.IN & 0x0F) | (PORTA.IN & 0xF0)))break;
+			
+		}
+		for(uint8_t b = 0; b < 3; b++){
 			while(dos);
 			cli();
 			AC0.INTCTRL = 0;
@@ -107,13 +121,11 @@ int main(void)
 			sei();
 			dos = true;
 		}
-		if(recv[0] == ((PORTC.IN & 0x0F) | (PORTA.IN & 0xF0))){
-			TCA0.SINGLE.CMP0 = recv[1];
-			TCA0.SINGLE.CMP1 = recv[2];
-			TCA0.SINGLE.CMP2 = recv[3];
-			TCA0.SINGLE.CTRLESET = (1 << 2); //reset timmer
-			recv[0] = 0; // clear as not to comre back here resetting the timmer again
-		}
+		
+		TCA0.SINGLE.CMP0 = recv[0];
+		TCA0.SINGLE.CMP1 = recv[1];
+		TCA0.SINGLE.CMP2 = recv[2];
+		TCA0.SINGLE.CTRLESET = (1 << 2); //reset timmer
 		/*char chr[30] = {0};
 		sprintf(chr, "%d", recv[1]);
 		for(uint8_t j = 0; j < sizeof(chr);j++){
